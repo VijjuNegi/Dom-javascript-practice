a1124793098be7fb21e18a6183e6af2475adf849
VijjuNegi <imvijju94negi@gmail.com>
2021-06-26 20:35:27 +0530

    Dom get methods getAttribute & attributes

--- DOM Methos/main.js
+++ DOM Methos/main.js
@@ -1,3 +1,10 @@
 // Dom get methods
 let textInner = document.getElementById("text").innerText;
 console.log(textInner);
+
+
+let getAtt = document.getElementById("text").getAttribute("class");
+console.log(getAtt);
+
+let attribute = document.getElementById("text").attributes[2].name;
+console.log(attribute);
