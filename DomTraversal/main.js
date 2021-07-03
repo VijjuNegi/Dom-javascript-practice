fb851d4e32e619f0eafd4f52b66dc50cad6872c9
VijjuNegi <imvijju94negi@gmail.com>
2021-07-03 20:34:37 +0530

    Dom Traversal firstElementChild & lastElementChild method

--- DomTraversal/main.js
+++ DomTraversal/main.js
@@ -8,3 +8,8 @@ console.log(prnt);
 let cldn = document.querySelector("#inner").children[1];
 document.querySelector("#inner").children[1].style.background = "#5f5f5f";
 console.log(cldn);
+
+
+// firstElementChild & lastElementChild
+let fstElecld = document.querySelector("#outer").firstElementChild.style.background = "#5f5f5f";
+console.log(fstElecld);
