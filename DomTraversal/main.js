421b0589a392fbde4d4c5801a98aa320efecdbea
VijjuNegi <imvijju94negi@gmail.com>
2021-07-03 20:29:07 +0530

    Dom Traversal children & childNodes method

--- DomTraversal/main.js
+++ DomTraversal/main.js
@@ -1,3 +1,10 @@
 // parentElement
 let prnt = document.querySelector("#child-c").parentElement.style.background="red";
 console.log(prnt);
+
+
+
+// children & childNodes
+let cldn = document.querySelector("#inner").children[1];
+document.querySelector("#inner").children[1].style.background = "#5f5f5f";
+console.log(cldn);
