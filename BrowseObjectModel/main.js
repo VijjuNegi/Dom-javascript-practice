df357fdd84522943ced12cce8c52b63c041e6513
vijjuNegi <imvijju94negi@gmail.com>
2021-07-14 10:04:52 +0530

    moveBy Window

--- BrowseObjectModel/main.js
+++ BrowseObjectModel/main.js
@@ -27,7 +27,7 @@ function closeWindow(){
     windows.close();
 }
 
-// move Window
+// moveTo Window
 var mywindows;
 function openWindows(){
     mywindows = window.open("","","width=600px,Height=300px,left=500px,top=300px");
@@ -35,4 +35,14 @@ function openWindows(){
 }
 function moveWindows(){
     mywindows.moveTo(100,100);
+}
+
+// moveBy Window
+var mywindows;
+function openWindows(){
+    mywindows = window.open("","","width=600px,Height=300px,left=500px,top=300px");
+    mywindows.document.write("<p>Hello jaaScript</p>");
+}
+function moveWindows(){
+    mywindows.moveBy(100,100);
 }
